d5896675f95a34c9b2e3c0cda34581386bbb1b4f
famutimine <62528290+famutimine@users.noreply.github.com>
2021-09-09 08:48:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ This threshold was calculated based on the value that optimized sensitivity and 
 This means that any patient with a mortality probability of 12% or higher is considered high risk for mortality within the next 24-48 hours.''')
 
 covid_df=pd.read_csv('https://raw.githubusercontent.com/famutimine/covid-prism/main/covid_data.csv',index_col=[0])
-X = covid_df.iloc[:, :-1]
-Y = covid_df.iloc[:, -1]
+X = covid_df.iloc[:, :-1].values
+Y = covid_df.iloc[:, -1].values
 st.sidebar.header('Please fill in current values')
 def user_input_features():
     input_features = {}
@@ -79,7 +79,7 @@ def explain_model_prediction(data):
 
 submit = st.sidebar.button('Get predictions')
 if submit:
-    probability = model.predict_proba(df)
+    probability = model.predict_proba(df)[:,1]
     st.header('Model Prediction')
     st.write("In-Hospital Mortality Probability: ", str(round(float(probability),3)*100) +"%")
     st.write('---')
