5a25fe20d47aa11d080fa721a24f21e8961c3257
famutimine <62528290+famutimine@users.noreply.github.com>
2021-12-20 22:07:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ if submit:
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(df.iloc[0])
     fig, ax = plt.subplots(nrows=1, ncols=1)
-    shap.plots._waterfall.waterfall_legacy(explainer.expected_value, shap_values[0])
+    shap.plots._waterfall.waterfall_legacy(explainer.expected_value, shap_values[0],feature_names=feature_names)
     st.pyplot(fig)
     st.write('''Variables in red increased the prediction while variables in blue decreased prediction for this patient. The magnitude of effect of each variable is indicated by the numerical value labels.''')
         
