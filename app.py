8ed92c25c096f43e8180d419d1ff93426fff2f5a
famutimine <62528290+famutimine@users.noreply.github.com>
2021-12-21 10:39:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ if submit:
     explainer = shap.Explainer(model,X)
     shap_values = explainer.shap_values(df.iloc[0])
     fig, ax = plt.subplots(nrows=1, ncols=1)
-    shap.plots._waterfall.waterfall_legacy(explainer.expected_value,shap_values,feature_names=feature_names)   
+    shap.plots.waterfall(shap_values) 
     st.pyplot(fig)
     st.write('''Variables corresponding to the red arrow increased the prediction while variables corresponding to the blue arrow decreased prediction for this patient. The magnitude of effect of each variable is indicated by the numerical value labels.''')
         
