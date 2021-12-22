0fe3061a1013db3d2d2eaceb15118b9d8c1aaf24
famutimine <62528290+famutimine@users.noreply.github.com>
2021-12-22 11:24:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ from flask import Flask, request, redirect, url_for, flash, jsonify, make_respon
 from PIL import Image
 from io import BytesIO
 
-@st.cache
+
 st.set_page_config(layout="centered")
 hide_streamlit_style = """
 <style>
