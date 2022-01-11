60f1ae4e3132c12373e96ccfbdaeb17e1a008c7d
famutimine <62528290+famutimine@users.noreply.github.com>
2022-01-10 18:29:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -47,8 +47,7 @@ Model achieved area under the receiver operating characteristic curve (AUROC) sc
 for predicting 24-hour risk, and AUROC score of 0.953, sensitivity of 87.6% and specificity of 92% for predicting 7-day risk.''')
 
 st.write('---')
-
-disclaimer_text = '<p style="font-family:Calibri; color:Black; font-size: 12px;">**Disclaimer:** This tool (hereinafter referred to as "COVID-PRISM / Algorithm") is being made publicly available for academic and research purposes only and is not intended for the diagnosis or treatment of any disease or condition, including COVID-19 in individual patients. COVID-PRISM is not a substitute for independent clinical assessment or judgement. All representations and warranties regarding the Algorithm, including warranties of fitness for use in clinical decision making and warranties that the Algorithm works as intended, is clinically safe, does not infringe on third party intellectual property rights, and/or is free from defects and bugs, are hereby disclaimed</p>'
+disclaimer_text = '<p style="font-family:Calibri; color:Black; font-size: 12px;"><b>Disclaimer:</b>This tool (hereinafter referred to as "COVID-PRISM / Algorithm") is being made publicly available for academic and research purposes only and is not intended for the diagnosis or treatment of any disease or condition, including COVID-19 in individual patients. COVID-PRISM is not a substitute for independent clinical assessment or judgement. All representations and warranties regarding the Algorithm, including warranties of fitness for use in clinical decision making and warranties that the Algorithm works as intended, is clinically safe, does not infringe on third party intellectual property rights, and/or is free from defects and bugs, are hereby disclaimed</p>'
 st.markdown(disclaimer_text, unsafe_allow_html=True)
 
 covid_df=pd.read_csv('https://raw.githubusercontent.com/famutimine/covid-prism/main/covid19_data.csv')
